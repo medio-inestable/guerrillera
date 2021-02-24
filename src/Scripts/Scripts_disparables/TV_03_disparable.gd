b6b63607919de323742702654f8d903642c925cf
centroCulturalKappa <anarkxpop@gmail.com>
2021-02-23 19:58:14 -0600

    se rompen pantallas gratis

--- src/Scripts/Scripts_disparables/TV_03_disparable.gd
+++ src/Scripts/Scripts_disparables/TV_03_disparable.gd
@@ -9,4 +9,8 @@ func _on_Area_input_event(camera, event, click_position, click_normal, shape_idx
 	if event is InputEventMouseButton:
 		#print(self)
 		if event.button_index == BUTTON_LEFT and event.pressed == true:
-			print(name)
+			$TV_03.visible = false
+			$TV_03_Rota.rotation.x = rand_range(-0.25,0.25)*PI
+			#$TV_03_Rota.rotation.y = 180
+			$TV_03_Rota.rotation.z = rand_range(-0.25,0.25)*PI
+			$TV_03_Rota.visible = true
